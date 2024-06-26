06e843641857f51cc7d12c1b020c8873376fb772
adrean paul sorono <adreanpaulsorono@gmail.com>
2024-06-26 18:16:49 +0800

    initial collission testing to spike

--- tank.gd
+++ tank.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+signal hit
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
@@ -19,8 +20,6 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
- 
-		
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -46,3 +45,11 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	#respawn in start
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+
